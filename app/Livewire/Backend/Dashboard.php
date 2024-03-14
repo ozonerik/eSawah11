614b20f07b4e18717fcc7e8e931d349a2e7f3395
ozonerik <ozonerik@gmail.com>
2024-03-14 09:19:42 +0700

    percobaan emit livewire3

--- app/Livewire/Backend/Dashboard.php
+++ app/Livewire/Backend/Dashboard.php
@@ -6,18 +6,29 @@ use Livewire\Component;
 use Livewire\Attributes\Layout;
 use App\Models\User;
 use Geocoder;
+use Livewire\Attributes\On; 
 
 class Dashboard extends Component
 {
+    
     #[Layout('layouts.app')] 
     public $user,$luas,$tanggal,$result;
     public $user_multi=[];
+
+    #[On('testEmit')] 
+    public function nilaiEmit($data)
+    {
+        dd($data);
+    }
+
     public function updatedLuas($value){
         $this->result=$value;
     }
+
     public function updatedTanggal($value){
         $this->result=$value;
     }
+
     public function mount()
     {
         $this->user = User::get();          
@@ -25,7 +36,7 @@ class Dashboard extends Component
 
     public function render()
     {
-        dd(Geocoder::getCoordinatesForAddress('Jakarta'));
+        //dd(Geocoder::getCoordinatesForAddress('Jakarta'));
         $this->dispatch('run_select2');
         return view('livewire.backend.dashboard');
     }
--- resources/views/livewire/backend/dashboard.blade.php
+++ resources/views/livewire/backend/dashboard.blade.php
@@ -1,3 +1,10 @@
+@push('js')
+    <script>
+        function myFunction() {
+            Livewire.dispatch('testEmit',['hallo']);
+        } 
+    </script>
+@endpush
 <div>
     <x-content_header name="Dashboard" >
         <li class="breadcrumb-item active">Dashboard</li>
@@ -5,6 +12,7 @@
     <div class="row mx-1">
         <x-card_form name="Daftar Lanja" width="12" order="1" smallorder="1" closeto="onRead">
             <h1>Ini Dashboard</h1>
+            <button onclick="myFunction()">Click Emit</button>
             <x-input_mask typemask="luas" disabled="false" ids="luas" label="Luas" types="text" name="luas" placeholder="Type Luas" />
             <x-input_mask typemask="tanggal" disabled="false" ids="tanggal" label="Tanggal" types="text" name="tanggal" placeholder="Type tanggal" />
             <x-input_mask typemask="text" disabled="true" ids="result" label="Result" types="text" name="result" placeholder="Type Result" />
