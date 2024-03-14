90a23dbd0c46185c199743a561ef7d7a8b73a3fc
ozonerik <ozonerik@gmail.com>
2024-03-14 19:04:07 +0700

    test blade leaflet-measure

--- resources/views/livewire/test.blade.php
+++ resources/views/livewire/test.blade.php
@@ -16,40 +16,45 @@
   <pre id="eventoutput">...</pre>
 </div>
 @push('js')
-  <script src="https://unpkg.com/leaflet@1.3.1/dist/leaflet.js" integrity="sha512-/Nsx9X4HebavoBvEBuyp3I7od5tA0UzAxs+j83KgC8PU0kgB4XiK4Lfe4y4cgBtaRJQEIFCW+oC506aPT2L1zw==" crossorigin=""></script>
-  <script src="https://ljagis.github.io/leaflet-measure/leaflet-measure.js"></script>
-  <script>
-    (function(L, document) {
-      var map = L.map('map', {
-        center: [29.749817, -95.080757],
-        zoom: 16,
-        measureControl: true
-      });
-      L.tileLayer('//server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
-        minZoom: 14,
-        maxZoom: 18,
-        attribution: '&copy; Esri &mdash; Sources: Esri, DigitalGlobe, Earthstar Geographics, CNES/Airbus DS, GeoEye, USDA FSA, USGS, Getmapping, Aerogrid, IGN, IGP, swisstopo, and the GIS User Community'
-      }).addTo(map);
+    <script src="https://unpkg.com/leaflet@1.3.1/dist/leaflet.js" integrity="sha512-/Nsx9X4HebavoBvEBuyp3I7od5tA0UzAxs+j83KgC8PU0kgB4XiK4Lfe4y4cgBtaRJQEIFCW+oC506aPT2L1zw==" crossorigin=""></script>
+    <script src="https://ljagis.github.io/leaflet-measure/leaflet-measure.js"></script>
+    <script>
+    document.addEventListener('livewire:initialized', () => {
+        loadMaps();
+    })
+    </script>
+    <script>
+        function loadMaps(){
+        var map = L.map('map', {
+            center: [29.749817, -95.080757],
+            zoom: 16,
+            measureControl: true
+        });
+        L.tileLayer('//server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
+            minZoom: 14,
+            maxZoom: 18,
+            attribution: '&copy; Esri &mdash; Sources: Esri, DigitalGlobe, Earthstar Geographics, CNES/Airbus DS, GeoEye, USDA FSA, USGS, Getmapping, Aerogrid, IGN, IGP, swisstopo, and the GIS User Community'
+        }).addTo(map);
 
-      map.on('measurefinish', function(evt) {
-        writeResults(evt);
-      });
+        map.on('measurefinish', function(evt) {
+            writeResults(evt);
+        });
 
-      function writeResults(results) {
-        document.getElementById('eventoutput').innerHTML = JSON.stringify(
-          {
-            area: results.area,
-            areaDisplay: results.areaDisplay,
-            lastCoord: results.lastCoord,
-            length: results.length,
-            lengthDisplay: results.lengthDisplay,
-            pointCount: results.pointCount,
-            points: results.points
-          },
-          null,
-          2
-        );
-      }
-    })(window.L, window.document);
-  </script>
+        function writeResults(results) {
+            document.getElementById('eventoutput').innerHTML = JSON.stringify(
+            {
+                area: results.area,
+                areaDisplay: results.areaDisplay,
+                lastCoord: results.lastCoord,
+                length: results.length,
+                lengthDisplay: results.lengthDisplay,
+                pointCount: results.pointCount,
+                points: results.points
+            },
+            null,
+            2
+            );
+        }
+        };
+    </script>
 @endpush
