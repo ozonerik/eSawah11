df24429dc4c2ce84a3ef18b4099d2553140a92b1
ozonerik <ozonerik@gmail.com>
2024-03-17 12:04:04 +0700

    stil not read id

--- resources/views/livewire/backend/sawahs.blade.php
+++ resources/views/livewire/backend/sawahs.blade.php
@@ -1,12 +1,12 @@
 @push('js')
 <script>
-    document.addEventListener('livewire:initialized', () => {
-        let input = document.getElementById('lokasi');
-        console.log(input);
+    document.addEventListener('livewire:navigated', () => {
+        console.log(document.getElementById('lokasi'));
     });
     Livewire.on('getLokasiSaatini', () => {
-        let input = document.getElementById('lokasiadd');
-        console.log(input);
+        console.log('getLokasiSaatini');
+        console.log(document.getElementById('lokasi'));
+        console.log(document.getElementById('lokasiadd'));
     });
 </script>
 @endpush
@@ -35,12 +35,12 @@
             </div>
         </x-card-section> 
         @if($mode=='read')
-        <input type="text" id="lokasi" name="lokasi" >
         <x-card_tablesawah type="primary" width="9" order="1" smallorder="1" title="Daftar Sawah" :data="$Sawah" :thead="['No Surat','Nama Sawah','Luas(m2)','Lokasi','Photo Sawah']" :tbody="['nosawah','namasawah','luas','lokasi','img']" :tbtn="['edit','del']" search="Search...">
         <x-slot:menu>
             <button wire:click="onAdd" class="btn btn-sm btn-primary" data-toggle="tooltip" title="Tambah"><i class="fas fa-plus"></i></button>
             <button wire:click="onDelSelect" class="btn btn-sm btn-danger" data-toggle="tooltip" title="Hapus" @if(empty($checked)) disabled @endif><i class="fas fa-trash"></i></button>
             <button wire:click="onTrashed" class="btn btn-sm btn-success" data-toggle="tooltip" title="Trash" @if(empty($Restoresawah->total())) disabled @endif><i class="fa fa-archive mr-2"></i>Trash</button>
+            Lokasi Read: <input type="text" id="lokasi" name="lokasi" >
         </x-slot>    
         </x-card_tablesawah>
         @elseif($mode=='trashed')
@@ -50,7 +50,7 @@
             <h4>Add Sawah Selected</h4>
             <x-slot:footer>
             <form wire:submit.prevent="addsawah">
-                <input type="text"  @created="$refresh" id="lokasiadd" name="lokasiadd" >
+                Lokasi Add: <input type="text"  @created="$refresh" id="lokasiadd" name="lokasiadd" >
                 <x-input_form wajib="true" disabled="" ids="nosawah" label="No Surat" types="text" name="nosawah" placeholder="Enter No Surat" />
                 <x-input_form wajib="true" disabled="" ids="namasawah" label="Nama Sawah" types="text" name="namasawah" placeholder="Type Nama Sawah" />
                 <x-input_form wajib="true" disabled="" ids="luas" label="Luas Sawah (m2)" types="text" name="luas" placeholder="Type Luas Sawah" />
