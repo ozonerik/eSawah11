a6858c22b12c88f5b1b4b4256346d30d85921e23
ozonerik <ozonerik@gmail.com>
2024-03-18 09:42:51 +0700

    percobaan maptest

--- resources/views/components/input_mask.blade.php
+++ resources/views/components/input_mask.blade.php
@@ -1,142 +1,180 @@
 <script>
-document.addEventListener('livewire:initialized', () => {
-    if('{{$typemask}}' === 'luas'){
-        $('#{{ $ids }}').inputmask({
-            'autoUnmask': true, 
-            'suffix': ' m2',
-            'alias': 'decimal', 
-            'radixPoint':',', 
-            'groupSeparator': '.', 
-            'autoGroup': true, 
-            'digits': 2, 
-            'digitsOptional': false, 
-            'rightAlign': false 
-        }).on('keyup', function(e) {
-            let nilai=$('#{{ $ids }}').val();
-            @this.set('{{ $name }}', nilai);
-        });
-    }else if('{{$typemask}}' === 'bata'){
-        $('#{{ $ids }}').inputmask({
-            'autoUnmask': true, 
-            'suffix': ' bata',
-            'alias': 'decimal', 
-            'radixPoint':',', 
-            'groupSeparator': '.', 
-            'autoGroup': true, 
-            'digits': 2, 
-            'digitsOptional': false, 
-            'rightAlign': false 
-        }).on('keyup', function(e) {
-            let nilai=$('#{{ $ids }}').val();
-            @this.set('{{ $name }}', nilai);
-        });
-    }else if('{{$typemask}}' === 'kwintal'){
-        $('#{{ $ids }}').inputmask({
-            'autoUnmask': true, 
-            'suffix': ' kw',
-            'alias': 'decimal', 
-            'radixPoint':',', 
-            'groupSeparator': '.', 
-            'autoGroup': true, 
-            'digits': 2, 
-            'digitsOptional': false, 
-            'rightAlign': false 
-        }).on('keyup', function(e) {
-            let nilai=$('#{{ $ids }}').val();
-            @this.set('{{ $name }}', nilai);
-        });
-    }else if('{{$typemask}}' === 'derajat'){
-        $('#{{ $ids }}').inputmask({
-            'autoUnmask': true, 
-            'suffix': ' °',
-            'alias': 'decimal', 
-            'radixPoint':',', 
-            'groupSeparator': '.', 
-            'autoGroup': true, 
-            'digits': 2, 
-            'digitsOptional': false, 
-            'rightAlign': false 
-        }).on('keyup', function(e) {
-            let nilai=$('#{{ $ids }}').val();
-            @this.set('{{ $name }}', nilai);
-        });
-    }else if('{{$typemask}}' === 'panjang'){
-        $('#{{ $ids }}').inputmask({
-            'autoUnmask': true, 
-            'suffix': ' m',
-            'alias': 'decimal', 
-            'radixPoint':',', 
-            'groupSeparator': '.', 
-            'autoGroup': true, 
-            'digits': 2, 
-            'digitsOptional': false, 
-            'rightAlign': false 
-        }).on('keyup', function(e) {
-            let nilai=$('#{{ $ids }}').val();
-            @this.set('{{ $name }}', nilai);
-        });
-    }else if('{{$typemask}}' === 'harga'){
-        $('#{{ $ids }}').inputmask({
-            'autoUnmask': true, 
-            'prefix': 'Rp ',
-            'alias': 'decimal', 
-            'shortcuts':{'r': '1000', 'j': '1000000','m':'1000000000','t':'1000000000000'},
-            'radixPoint':',', 
-            'groupSeparator': '.', 
-            'autoGroup': true, 
-            'digits': 0, 
-            'digitsOptional': false, 
-            'rightAlign': false 
-        }).on('keyup', function(e) {
-            let nilai=$('#{{ $ids }}').val();
-            @this.set('{{ $name }}', nilai);
-        });
-    }else if('{{$typemask}}' === 'desimal'){
-        $('#{{ $ids }}').inputmask({
-            'autoUnmask': true, 
-            'alias': 'decimal', 
-            'radixPoint':',', 
-            'groupSeparator': '.', 
-            'autoGroup': true, 
-            'digits': 2, 
-            'digitsOptional': false, 
-            'rightAlign': false 
-        }).on('keyup', function(e) {
-            let nilai=$('#{{ $ids }}').val();
-            @this.set('{{ $name }}', nilai);
-        });
-    }else if('{{$typemask}}' === 'telp'){
-        $('#{{ $ids }}').inputmask({
-            'autoUnmask': true, 
-            'mask': ['9999-9999-999[9][9][9]'],
-            'rightAlign': false 
-        }).on('keyup', function(e) {
-            let nilai=$('#{{ $ids }}').val();
-            @this.set('{{ $name }}', nilai);
-        });
-    }else if('{{$typemask}}' === 'tanggal'){
-        $('#{{ $ids }}').datepicker({
-            autoclose:true,
-            format:'dd/mm/yyyy',
-            orientation:'bottom',
-            highlight:true,
-            language:'id',
-            todayHighlight:true,
-            todayBtn:true,
-        }).on('changeDate', function(e) {
-            let nilai=$('#{{ $ids }}').val();
-            @this.set('{{ $name }}', nilai);
-        });
-        $('#{{ $ids }}').inputmask({
-            'alias': 'datetime', 
-            'inputFormat': 'dd/mm/yyyy',
-            'rightAlign': false 
-        }).on('keyup', function(e) {
-            let nilai=$('#{{ $ids }}').val();
-            @this.set('{{ $name }}', nilai);
-        });
-    }
-
+document.addEventListener('livewire:init', () => {
+    Livewire.hook('element.init', ({ component, el }) => {
+        if('{{$typemask}}' === 'luas'){
+            $('#{{ $ids }}').inputmask({
+                'autoUnmask': true, 
+                'suffix': ' m2',
+                'alias': 'decimal',
+                'onBeforeMask': function (value) {
+                    value=0;
+                    return value;
+                }, 
+                'radixPoint':',', 
+                'groupSeparator': '.', 
+                'autoGroup': true, 
+                'digits': 2, 
+                'digitsOptional': false, 
+                'rightAlign': false 
+            }).on('keyup', function(e) {
+                let nilai=$('#{{ $ids }}').val();
+                @this.set('{{ $name }}', nilai);
+            });
+        }else if('{{$typemask}}' === 'bata'){
+            $('#{{ $ids }}').inputmask({
+                'autoUnmask': true, 
+                'suffix': ' bata',
+                'alias': 'decimal',
+                'onBeforeMask': function (value) {
+                    value=0;
+                    return value;
+                }, 
+                'radixPoint':',', 
+                'groupSeparator': '.', 
+                'autoGroup': true, 
+                'digits': 2, 
+                'digitsOptional': false, 
+                'rightAlign': false 
+            }).on('keyup', function(e) {
+                let nilai=$('#{{ $ids }}').val();
+                @this.set('{{ $name }}', nilai);
+            });
+        }else if('{{$typemask}}' === 'kwintal'){
+            $('#{{ $ids }}').inputmask({
+                'autoUnmask': true, 
+                'suffix': ' kw',
+                'alias': 'decimal', 
+                'onBeforeMask': function (value) {
+                    value=0;
+                    return value;
+                },
+                'radixPoint':',', 
+                'groupSeparator': '.', 
+                'autoGroup': true, 
+                'digits': 2, 
+                'digitsOptional': false, 
+                'rightAlign': false 
+            }).on('keyup', function(e) {
+                let nilai=$('#{{ $ids }}').val();
+                @this.set('{{ $name }}', nilai);
+            });
+        }else if('{{$typemask}}' === 'derajat'){
+            $('#{{ $ids }}').inputmask({
+                'autoUnmask': true, 
+                'suffix': ' °',
+                'alias': 'decimal', 
+                'onBeforeMask': function (value) {
+                    value=0;
+                    return value;
+                },
+                'radixPoint':',', 
+                'groupSeparator': '.', 
+                'autoGroup': true, 
+                'digits': 2, 
+                'digitsOptional': false, 
+                'rightAlign': false 
+            }).on('keyup', function(e) {
+                let nilai=$('#{{ $ids }}').val();
+                @this.set('{{ $name }}', nilai);
+            });
+        }else if('{{$typemask}}' === 'panjang'){
+            $('#{{ $ids }}').inputmask({
+                'autoUnmask': true, 
+                'suffix': ' m',
+                'alias': 'decimal', 
+                'onBeforeMask': function (value) {
+                    value=0;
+                    return value;
+                },
+                'radixPoint':',', 
+                'groupSeparator': '.', 
+                'autoGroup': true, 
+                'digits': 2, 
+                'digitsOptional': false, 
+                'rightAlign': false 
+            }).on('keyup', function(e) {
+                let nilai=$('#{{ $ids }}').val();
+                @this.set('{{ $name }}', nilai);
+            });
+        }else if('{{$typemask}}' === 'harga'){
+            $('#{{ $ids }}').inputmask({
+                'autoUnmask': true, 
+                'prefix': 'Rp ',
+                'alias': 'decimal', 
+                'onBeforeMask': function (value) {
+                    value=0;
+                    return value;
+                },
+                'shortcuts':{'r': '1000', 'j': '1000000','m':'1000000000','t':'1000000000000'},
+                'radixPoint':',', 
+                'groupSeparator': '.', 
+                'autoGroup': true, 
+                'digits': 0, 
+                'digitsOptional': false, 
+                'rightAlign': false 
+            }).on('keyup', function(e) {
+                let nilai=$('#{{ $ids }}').val();
+                @this.set('{{ $name }}', nilai);
+            });
+        }else if('{{$typemask}}' === 'desimal'){
+            $('#{{ $ids }}').inputmask({
+                'autoUnmask': true, 
+                'alias': 'decimal', 
+                'onBeforeMask': function (value) {
+                    value=0;
+                    return value;
+                },
+                'radixPoint':',', 
+                'groupSeparator': '.', 
+                'autoGroup': true, 
+                'digits': 2, 
+                'digitsOptional': false, 
+                'rightAlign': false 
+            }).on('keyup', function(e) {
+                let nilai=$('#{{ $ids }}').val();
+                @this.set('{{ $name }}', nilai);
+            });
+        }else if('{{$typemask}}' === 'telp'){
+            $('#{{ $ids }}').inputmask({
+                'autoUnmask': true, 
+                'mask': ['9999-9999-999[9][9][9]'],
+                'onBeforeMask': function (value) {
+                    value=0;
+                    return value;
+                },
+                'rightAlign': false 
+            }).on('keyup', function(e) {
+                let nilai=$('#{{ $ids }}').val();
+                @this.set('{{ $name }}', nilai);
+            });
+        }else if('{{$typemask}}' === 'tanggal'){
+            $('#{{ $ids }}').datepicker({
+                autoclose:true,
+                format:'dd/mm/yyyy',
+                orientation:'bottom',
+                highlight:true,
+                language:'id',
+                todayHighlight:true,
+                todayBtn:true,
+            }).on('changeDate', function(e) {
+                let nilai=$('#{{ $ids }}').val();
+                @this.set('{{ $name }}', nilai);
+            });
+            $('#{{ $ids }}').inputmask({
+                'alias': 'datetime', 
+                'inputFormat': 'dd/mm/yyyy',
+                'rightAlign': false ,
+                'onBeforeMask': function (value) {
+                    let date = new Date().toLocaleDateString("id-ID");
+                    value=date;
+                    return value;
+                },
+            }).on('keyup', function(e) {
+                let nilai=$('#{{ $ids }}').val();
+                @this.set('{{ $name }}', nilai);
+            });
+        }
+    })
 })
 </script>
 <div class="form-group">
